3bb322f7f2c19c61fb0b4157c3191c5fc5506623
CYYSILVER <immortal_youth@126.com>
2019-08-01 10:07:14 +0800

    seperate prod & dev

--- webpack.config.js
+++ webpack.config.js
@@ -1,90 +1,94 @@
 const path = require('path')
 const webpack = require('webpack')
 
-/*
- * SplitChunksPlugin is enabled by default and replaced
- * deprecated CommonsChunkPlugin. It automatically identifies modules which
- * should be splitted of chunk by heuristics using module duplication count and
- * module category (i. e. node_modules). And splits the chunks…
- *
- * It is safe to remove "splitChunks" from the generated configuration
- * and was added as an educational example.
- *
- * https://webpack.js.org/plugins/split-chunks-plugin/
- *
- */
+// /*
+//  * SplitChunksPlugin is enabled by default and replaced
+//  * deprecated CommonsChunkPlugin. It automatically identifies modules which
+//  * should be splitted of chunk by heuristics using module duplication count and
+//  * module category (i. e. node_modules). And splits the chunks…
+//  *
+//  * It is safe to remove "splitChunks" from the generated configuration
+//  * and was added as an educational example.
+//  *
+//  * https://webpack.js.org/plugins/split-chunks-plugin/
+//  *
+//  */
 
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-/*
- * We've enabled HtmlWebpackPlugin for you! This generates a html
- * page for you when you compile webpack, which will make you start
- * developing and prototyping faster.
- *
- * https://github.com/jantimon/html-webpack-plugin
- *
- */
+// /*
+//  * We've enabled HtmlWebpackPlugin for you! This generates a html
+//  * page for you when you compile webpack, which will make you start
+//  * developing and prototyping faster.
+//  *
+//  * https://github.com/jantimon/html-webpack-plugin
+//  *
+//  */
 
-module.exports = {
-	mode: 'development',
-  entry: './src/index.js',
-  // devtool: 'source-map',
-  output: {
-    filename: 'ajaxp.js',
-    path: path.resolve(__dirname, 'dist'),
-    sourceMapFilename: 'ajaxp.map',
-    libraryTarget: 'umd',
-    library: 'ajaxp'
-  },
+module.exports = (env) => {
 
-  plugins: [
-    new webpack.ProgressPlugin(),
-    new HtmlWebpackPlugin({
-			filename: 'index.html',
-			template: 'public/index.html'
-		})
-  ],
+  let name = env == 'production' ? 'ajaxp.min' : 'ajaxp'
+  return {
+    mode: env,
+    entry: './src/index.js',
+    // devtool: 'source-map',
+    output: {
+      filename: `${name}.js`,
+      path: path.resolve(__dirname, 'dist'),
+      sourceMapFilename: `${name}.map`,
+      libraryTarget: 'umd',
+      library: 'ajaxp'
+    },
 
-  module: {
-    rules: [
-      {
-        test: /.(js|jsx)$/,
-        include: [path.resolve(__dirname, 'src')],
-        loader: 'babel-loader',
+    plugins: [
+      new webpack.ProgressPlugin(),
+      new HtmlWebpackPlugin({
+        filename: 'index.html',
+        template: 'public/index.html'
+      })
+    ],
 
-        options: {
-          plugins: ['syntax-dynamic-import'],
+    module: {
+      rules: [
+        {
+          test: /.(js|jsx)$/,
+          include: [path.resolve(__dirname, 'src')],
+          loader: 'babel-loader',
 
-          presets: [
-            [
-              '@babel/preset-env',
-              {
-                modules: false
-              }
+          options: {
+            plugins: ['syntax-dynamic-import'],
+
+            presets: [
+              [
+                '@babel/preset-env',
+                {
+                  modules: false
+                }
+              ]
             ]
-          ]
+          }
         }
-      }
-    ]
-  },
+      ]
+    },
 
-  // optimization: {
-  //   splitChunks: {
-  //     cacheGroups: {
-  //       vendors: {
-  //         priority: -10,
-  //         test: /[\\/]node_modules[\\/]/
-  //       }
-  //     },
+    // optimization: {
+    //   splitChunks: {
+    //     cacheGroups: {
+    //       vendors: {
+    //         priority: -10,
+    //         test: /[\\/]node_modules[\\/]/
+    //       }
+    //     },
 
-  //     chunks: 'async',
-  //     minChunks: 1,
-  //     minSize: 30000,
-  //     name: true
-  //   }
-  // },
+    //     chunks: 'async',
+    //     minChunks: 1,
+    //     minSize: 30000,
+    //     name: true
+    //   }
+    // },
 
-  devServer: {
-    open: true
+    devServer: {
+      open: true
+    }
   }
 }
