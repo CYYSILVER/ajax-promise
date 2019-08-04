e8fc0cf96639a9d043bd24ca44254b35190bdbe5
CYYSILVER <immortal_youth@126.com>
2019-08-04 23:37:40 +0800

    move send function to core/index.js

---
+++ src/core/index.js
@@ -0,0 +1,56 @@
+const { convertIfJson } = require('../helpers/util')
+
+module.exports = {
+  send(options) {
+    return new Promise((resolve, reject) => {
+      let xhr = new XMLHttpRequest()
+      xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4) {
+          if (xhr.status >= 200 && xhr.status < 300) {
+            // 将响应封装为对象
+            let headersObj = {}
+            let headers = xhr.getAllResponseHeaders()
+            headers
+              .trim('\n')
+              .split('\n')
+              .forEach((param, index) => {
+                let matches = param.match(/(.*?): (.*)/)
+                let key = matches[1]
+                let value = matches[2]
+                headersObj[key] = value
+              })
+
+            let response = {
+              data: convertIfJson(xhr.response), // 转换为Object，如果为Json格式的h话
+              config: options,
+              status: xhr.status,
+              statusText: xhr.statusText,
+              headers: headersObj,
+              request: xhr
+            }
+
+            resolve(response)
+          } else {
+            reject(new Error(`Request failed with status code ${xhr.status}`))
+          }
+        }
+      }
+
+      // 获取参数
+      let { method, async, url, data, timeout } = options
+      // 将传入参数转化为查询字符
+      let params = JSON.stringify(data)
+      method = method.toUpperCase()
+      xhr.timeout = timeout
+      if (method.match(/^(?:GET|DELETE|HEAD)$/)) {
+        xhr.open(method, `${url}?${params}`, async)
+        xhr.send(null)
+      } else if (method.match(/^(?:POST|PUT|PATCH)/)) {
+        xhr.open(method, url, async)
+        xhr.setRequestHeader('Content-Type', 'application/json')
+        xhr.send(params)
+      }
+      // TODO: method不支持错误处理
+    })
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const { convertIfJson } = require('./helpers/util')
+const { send } = require('./core/index')
 
 module.exports = /** @class */ (function() {
   /**
@@ -7,8 +7,10 @@ module.exports = /** @class */ (function() {
    * @param {Obejct} options ajax请求配置
    * @returns {Promise}
    */
-  function ajax(options) {
-    options = createDefaultOptions(options)
+  function ajax(param1, param2) {
+    let options = param2 || param1
+    let url = param2 ? param1 : ""
+    options = createOptions(options, url)
     return send(options)
   }
 
@@ -23,20 +25,17 @@ module.exports = /** @class */ (function() {
    * @returns {Promise}
    */
   ajax.get = function(url, options) {
-    options = createDefaultOptions(options)
-    options.url = url
+    options = createOptions(options, url)
     options.method = 'GET'
     return send(options)
   }
   ajax.delete = function(url, options) {
-    options = createDefaultOptions(options)
-    options.url = url
+    options = createOptions(options, url)
     options.method = 'DELETE'
     return send(options)
   }
   ajax.head = function(url, options) {
-    options = createDefaultOptions(options)
-    options.url = url
+    options = createOptions(options, url)
     options.method = 'HEAD'
     return send(options)
   }
@@ -50,94 +49,31 @@ module.exports = /** @class */ (function() {
    * @returns {Promise}
    */
   ajax.post = function(url, data, options) {
-    options = createDefaultOptions(options)
-    options.data = data
-    options.url = url
+    options = createOptions(options, url, data)
     options.method = 'POST'
     return send(options)
   }
   ajax.put = function(url, data, options) {
-    options = createDefaultOptions(options)
-    options.data = data
-    options.url = url
+    options = createOptions(options, url, data)
     options.method = 'PUT'
     return send(options)
   }
   ajax.patch = function(url, data, options) {
-    options = createDefaultOptions(options)
-    options.data = data
-    options.url = url
+    options = createOptions(options, url, data)
     options.method = 'PATCH'
     return send(options)
   }
   
-
-  /**
-   * 用于封装xhr对象，发送请求，返回promise对象
-   *
-   * @param {Object} options ajaxq请求配置
-   * @return {Promise}  promised对象
-   */
-  function send(options) {
-    return new Promise((resolve, reject) => {
-      let xhr = new XMLHttpRequest()
-      xhr.onreadystatechange = function() {
-        if (xhr.readyState == 4) {
-          if (xhr.status >= 200 && xhr.status < 300) {
-            // 将响应封装为对象
-            let headersObj = {}
-            let headers = xhr.getAllResponseHeaders()
-            headers
-              .trim('\n')
-              .split('\n')
-              .forEach((param, index) => {
-                let matches = param.match(/(.*?): (.*)/)
-                let key = matches[1]
-                let value = matches[2]
-                headersObj[key] = value
-              })
-              
-            let response = {
-              data: convertIfJson(xhr.response), // 转换为Object，如果为Json格式的h话
-              config: options,
-              status: xhr.status,
-              statusText: xhr.statusText,
-              headers: headersObj,
-              request: xhr
-            }
-
-            resolve(response)
-          } else {
-            reject(new Error(`Request failed with status code ${xhr.status}`))
-          }
-        }
-      }
-
-      // 获取参数
-      let { method, async, url, data, timeout } = options
-      // 将传入参数转化为查询字符
-      let params = JSON.stringify(data)
-      method = method.toUpperCase()
-      xhr.timeout = timeout
-      if (method.match(/^(?:GET|DELETE|HEAD)$/)) {
-        xhr.open(method, `${url}?${params}`, async)
-        xhr.send(null)
-      } else if (method.match(/^(?:POST|PUT|PATCH)/)){
-        xhr.open(method, url, async)
-        xhr.setRequestHeader('Content-Type', 'application/json')
-        xhr.send(params)
-      }
-      // TODO: method不支持错误处理
-    })
-  }
-
   /**
    * 混合默认ajax配置与新的ajax配置
    *
    * @param {*} options 新的配置
    * @returns {Object} 混合后的配置
    */
-  function createDefaultOptions(options, url, data) {
+  function createOptions(options, url, data) {
+    options = options || {}
+    options.url = url
+    options.data = data
     options = Object.assign(
       {
         method: 'GET',
